848806e195685b412315e954d4f21e438c8c5d37
YAZILIM FURYASI <yazilimfuryasi@gmail.com>
2022-12-19 02:02:13 +0300

    v2

--- interview/main/views.py
+++ interview/main/views.py
@@ -4,11 +4,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+import os, json
+
+def ROOT():
+    DATA = []
+    for file in os.listdir(r"main/nft"):
+        if file.endswith(".json"):
+            DATA.append(json.load(open("main/nft/"+file, "r")))
+            img = DATA[-1]["image"]
+            DATA[-1]["image"] = "https://ipfs.io/ipfs/" + img.split('ipfs://')[1]
+    return DATA
 
 def index(request):
     if request.user.is_authenticated:
         return redirect("dashboard")
-
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -40,7 +49,8 @@ def register(request):
 
 @login_required(login_url="index")
 def dashboard(request):
-    DATA = {"user": request.user}
+    DATA = {"user": request.user,
+            "data": ROOT()}
     return render(request, "dashboard.html", DATA)
 
 @login_required(login_url="index")
